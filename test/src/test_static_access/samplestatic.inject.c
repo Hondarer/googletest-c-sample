5023dbf6c9c45fc169f2ce99ae5e13523f270770
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-02-22 20:48:04 +0900

    ソースの丁寧化

--- test/src/test_static_access/samplestatic.inject.c
+++ test/src/test_static_access/samplestatic.inject.c
@@ -1,6 +1,7 @@
+// テスト対象ソースファイルの注入用追加ソース
 // このソースはテスト対象の末尾に結合されるので
 // static メンバーへのアクセサーを記載することで
-// テストプログラムから static メンバーにアクセスできるようになる
+// テストプログラムから static メンバーにアクセスできる
 #ifndef _IN_TEST_FRAMEWORK_
 #include "samplestatic.c"
 #endif // _IN_TEST_FRAMEWORK_
--- test/src/test_static_access/samplestatic.inject.h
+++ test/src/test_static_access/samplestatic.inject.h
@@ -1,5 +1,8 @@
-#ifndef _INJECT_H
-#define _INJECT_H
+// テスト対象ソースファイルの注入用追加ソースのヘッダ
+// このヘッダをテストプログラムが参照することで
+// テスト対象ソースの static メンバにアクセスできる
+#ifndef _SAMPLESTATIC_INJECT_H
+#define _SAMPLESTATIC_INJECT_H
 
 #ifdef __cplusplus
 extern "C"
@@ -12,4 +15,4 @@ extern "C"
 }
 #endif
 
-#endif // _INJECT_H
+#endif // _SAMPLESTATIC_INJECT_H
