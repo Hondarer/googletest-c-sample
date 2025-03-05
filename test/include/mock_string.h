4fb4781079decca8204ff2079f7d8b86604f0e04
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-03-06 06:19:19 +0900

    support_caller_string.h

--- test/include/mock_string.h
+++ test/include/mock_string.h
@@ -8,15 +8,15 @@ extern "C"
 {
 #endif
 
-    extern void *mock_memset(void *, int, size_t);
+    extern void *mock_memset(const char *, const int, const char *, void *, int, size_t);
 
-#ifdef __cplusplus  
+#ifdef __cplusplus
 }
 #endif
 
 #ifdef _IN_OVERRIDE_HEADER_STRING_H_
 
-#define memset(s, c, n) mock_memset(s, c, n)
+#define memset(s, c, n) mock_memset(__FILE__, __LINE__, __func__, s, c, n)
 
 #else // _IN_OVERRIDE_HEADER_STRING_H_
 
@@ -29,7 +29,7 @@ extern void *delegate_real_memset(void *, int, size_t);
 class Mock_string
 {
 public:
-MOCK_METHOD(void *, memset, (void *, int, size_t));
+    MOCK_METHOD(void *, memset, (const char *, const int, const char *, void *, int, size_t));
 
     Mock_string();
     ~Mock_string();
